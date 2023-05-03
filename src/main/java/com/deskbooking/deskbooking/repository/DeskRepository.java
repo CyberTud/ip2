81e37d42192e9c7afd683a49d95e409af3c19205
Olaf4tza <73993173+DragosSofia@users.noreply.github.com>
2023-05-03 13:55:37 +0300

    Fixing booking problems

--- src/main/java/com/deskbooking/deskbooking/repository/DeskRepository.java
+++ src/main/java/com/deskbooking/deskbooking/repository/DeskRepository.java
@@ -1,5 +1,6 @@
 package com.deskbooking.deskbooking.repository;
 
+import com.deskbooking.deskbooking.model.Desk;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeskRepository extends JpaRepository<Desk,Integer> {
